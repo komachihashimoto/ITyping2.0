c417c0c2eaa7521eba45f4c22ea260ec15d9ad26
komachihashimoto <joujinameko@gmail.com>
2023-11-02 10:46:41 +0900

    newページの作成（途中まで）

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,11 +3,23 @@ class GamesController < ApplicationController
   end
 
   def new
+    @rank = Rank.new
+    @score = session[:score]
   end
 
   def create
+    @rank = Rank.new(rank_params)
+    @rank.score = @score
+    if @rank.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
-  def show
+  private
+
+  def rank_params
+    params.require(rank).permit(:nickname, :score, :country_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-resources :games, only:[:index, :new, :create, :show]
+resources :games, only:[:index, :new, :create]
 root to: 'games#index'
 end
