4dbf8367ddaba23d19739859e7b63b0f5efd32b1
komachihashimoto <joujinameko@gmail.com>
2023-11-07 17:06:52 +0900

    ランキング機能

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,25 +1,22 @@
 class GamesController < ApplicationController
   def index
-  end
-
-  def new
     @rank = Rank.new
-    @score = session[:score]
+    @rank = Rank.order(score: :desc)
   end
 
   def create
     @rank = Rank.new(rank_params)
-    @rank.score = @score
     if @rank.save
       redirect_to root_path
     else
       render :new
+      puts @rank.errors.full_messages
     end
   end
 
   private
 
   def rank_params
-    params.require(rank).permit(:nickname, :score, :country_id)
+    params.require(:rank).permit(:nickname, :score, :country_id)
   end
 end
